0474182f9046429f055572b1c902806284c0337e
pgallazzi <pablogallazzi@gmail.com>
2016-04-23 19:39:21 -0300

    Refactor, borro código innecesario

--- src/tp_tadp.rb
+++ src/tp_tadp.rb
@@ -2,98 +2,59 @@ module Matcher
   def val(value)
     ValPattern.new value
   end
-
   def type(value)
     TypePattern.new value
   end
 end
 
-
 class Combinator
-  attr_accessor :one, :another
-
-  def and(one, another)
-    AndCombinator.new(one, another)
+  def and(other)
+    AndCombinator.new(self, other)
   end
-
-  def or (one, another)
-    OrCombinator.new(one, another)
+  def or(other)
+    OrCombinator.new(self, other)
   end
 end
 
 class ValPattern < Combinator
   attr_accessor :val
-
   def initialize(val)
     self.val = val
   end
-
   def call(value)
     self.val.eql? value
-  end
-
-  def and(other)
-    AndCombinator.new(self, other)
-  end
-  def or(other)
-    OrCombinator.new(self, other)
   end
 end
 
 class TypePattern < Combinator
   attr_accessor :type
-
   def initialize(type)
     self.type = type
   end
-
   def call(type)
     type.is_a? self.type
   end
-
-  def and(other)
-    AndCombinator.new(self, other)
-  end
-  def or(other)
-    OrCombinator.new(self, other)
-  end
 end
 
-class AndCombinator
+class AndCombinator < Combinator
   attr_accessor :one, :another
-
   def initialize(one, another)
     self.one = one
     self.another = another
   end
-
   def call(val)
     one.call(val) && another.call(val)
   end
-  def and(other)
-    AndCombinator.new(self, other)
-  end
-  def or(other)
-    OrCombinator.new(self, other)
-  end
 end
 
-class OrCombinator
+class OrCombinator < Combinator
   attr_accessor :one, :another
-
   def initialize(one, another)
     self.one = one
     self.another = another
   end
-
   def call(val)
     one.call(val) || another.call(val)
-  end
-  def and(other)
-    AndCombinator.new(self, other)
-  end
-  def or(other)
-    OrCombinator.new(self, other)
   end
 end
 
@@ -103,12 +64,15 @@ class Pinga
   def a
     val(5).and(type(Fixnum)).call(5)
   end
+
   def a1
-    val("Hola").and(type(String)).and(val("Chau")).call("Hola")
+    val("Hola").and(type(String)).and(val("Hola")).call("Hola")
   end
+
   def a2
     val("Hola").and(type(String)).call("Hola")
   end
+
   def b
     type(Fixnum).call(5)
   end
